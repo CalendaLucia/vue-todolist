d7f6f6280f508258711e15482ae146085ec23ce3
CalendaLucia <calendalucia4@gmail.com>
2023-01-24 16:58:00 +0100

    fine

--- js/scripts.js
+++ js/scripts.js
@@ -3,20 +3,62 @@ const { createApp } = Vue
   createApp({
     data() {
       return {
-        todos: [
-            'Leggere un libro',
-            'Fare la spesa',
-            'Giocare alla playstation',
-            
+
+        newTodo: [],
+
+        todos:  [
+            {
+               todo:  'Leggere un libro',
+               done: true
+            },
+
+            {
+               todo :    'Fare la spesa',
+               done: false
+            },
+
+            {
+               todo:    'Giocare alla playstation',
+               done:false
+            }
 
         ]
       }
     },
     methods: {
+
+        remove(index) {
+
+            this.todos.splice(index, 1)
+        },
+
+        addTask() {
+
+            if(this.newTodo != '' && this.newTodo.length > 3) {
+                this.todos.push({
+                    todo: this.newTodo,
+                    done: false
+                });
+                this.newTodo = '';
+
+            }
+
+        },
+
+
+        submitOnEnter(event) {
+            if(event.keyCode === 13) {
+                this.addTask()
+            }
+        },
+
+        toggleDone(index) {
+
+            
+            this.todos[index].done = !this.todos[index].done;
+        }
+
         
     },
   }).mount('#app')
 
-//   MILESTONE 1
-// Stampare all'interno di una lista HTML un item per ogni todo.
-// Se la proprietà done è uguale a true, visualizzare il testo del todo sbarrato.
